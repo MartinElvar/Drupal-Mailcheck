e8025a30da9c3e63a068c69dde112fbafc30f6ec
Martin Elvar <faktaposen@gmail.com>
2012-03-24 17:20:41 +0100

    Added click function, to correct the typo.

--- js/mailchecker.js
+++ js/mailchecker.js
@@ -3,13 +3,22 @@ jQuery(function($) {
   var domains = Drupal.settings.mailcheck['domains'];
 
   $('.mailcheck').blur( function() {
+    var currentform = this;
+
     $(this).mailcheck({
       domains: domains,
       suggested: function(element, suggestion) {
 
         message = Drupal.settings.mailcheck.message;
-
         $('.mailcheck-action').html(message.replace('[corrected-mail]', '<span class="corrected-mail">' + suggestion.full + '</span>'));     
+        
+        $('.corrected-mail').click(function() {
+    
+          $(this).parent().hide();
+          $(currentform).val(suggestion.full);
+    
+        });
+ 
       },
       empty: function(element) {
         console.log(element);
