021144dfd873876209d33a3f47f203b1b9e1f088
Martin Elvar <faktaposen@gmail.com>
2012-03-25 00:13:13 +0100

    Show in, now works properly.

--- js/mailchecker.js
+++ js/mailchecker.js
@@ -5,20 +5,20 @@ jQuery(function($) {
   $('.mailcheck').blur( function() {
     var currentform = this;
 
-    if(Drupal.settings.mailcheck.show_in_des == true) {
-      $(this).parent().find('.description').addClass('mailcheck-action');
-    }
-
     $(this).mailcheck({
       domains: domains,
       suggested: function(element, suggestion) {
+
+        if(Drupal.settings.mailcheck.show_in_des == true) {
+          $(this).parent().find('.description').prepend('<div class="mailcheck-action"></div>');
+        }
 
         message = Drupal.settings.mailcheck.message;
         $('.mailcheck-action').html(message.replace('[corrected-mail]', '<span class="corrected-mail">' + suggestion.full + '</span>'));     
         
         $('.corrected-mail').click(function() {
     
-          $(this).parent().hide();
+          $(this).parent().html("");
           $(currentform).val(suggestion.full);
     
         });
