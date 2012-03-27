784711dd6deaac6b8c4795ef5f6b5caa2cd09722
Martin Elvar <faktaposen@gmail.com>
2012-03-27 15:16:52 +0200

    Added awesome shake gesture

--- js/mailchecker.js
+++ js/mailchecker.js
@@ -16,6 +16,10 @@ jQuery(function($) {
         message = Drupal.settings.mailcheck.message;
         $('.mailcheck-action').html(message.replace('[corrected-mail]', '<span class="corrected-mail">' + suggestion.full + '</span>'));     
         
+        if(Drupal.settings.mailcheck.shake) {
+          skaheThatShit($('.mailcheck-action')); 
+        }
+
         $('.corrected-mail').click(function() {
     
           $(this).parent().html("");
@@ -30,4 +34,27 @@ jQuery(function($) {
         
     }); 
   });
+  
+  function skaheThatShit(el) {
+
+    // First set attributes
+    $(el).css({
+      'position' : 'relative',
+      'left' : '0px',
+    });
+
+    // Then animate
+
+    $(el).animate({opacity: 0.25}, 100);
+    $(el).animate({left: '-=10'},100); //these 4 lines
+    $(el).animate({left: '+=20'},100); //are the main
+    $(el).animate({left: '-=20'},100); //code that cause
+    $(el).animate({left: '+=10'},100); //the shaking
+    $(el).animate({opacity: 1.00}, 170);
+
+    // bring back settings
+  } 
+
 });
+
+
